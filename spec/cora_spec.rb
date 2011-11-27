75b6d9c6abef7e34d751f6f2d03b2876c881ad62
Jack Chen (chendo) <me@chen.do>
2011-11-27 21:08:09 +1100

    Moved TestPlugin into its own file

--- spec/cora_spec.rb
+++ spec/cora_spec.rb
@@ -1,108 +1,8 @@
 require 'spec_helper'
+require 'support/test_plugin'
 
 describe Cora do
   context "integration test" do
-
-    class TestPlugin < Cora::Plugin
-
-      listen_for /test/ do
-        say "test!"
-      end
-
-      listen_for /^foo$/ do
-        say "foo"
-        set_state :waiting_for_bar
-      end
-
-      listen_for /send message/ do
-        receipent = ask "Who should I send it to?"
-        say "Sending message to #{receipent}"
-      end
-
-      listen_for /ask multiple/ do
-        answer = ask "Question 1"
-        say "You said: #{answer}"
-        answer = ask "Question 2"
-        say "You said: #{answer}"
-        answer = ask "Question 3"
-        say "You said: #{answer}"
-      end
-
-      listen_for /confirm something/ do
-        confirm "Does confirm work?" do |confirmed|
-          if confirmed 
-            say "Confirmed"
-          else
-            say "Canceled"
-          end
-        end
-      end
-
-      listen_for /confirm custom reprompt/ do
-        confirm "Does confirm work?","What you say!?" do |confirmed|
-          if confirmed 
-            say "Confirmed"
-          else
-            say "Canceled"
-          end
-        end
-      end
-
-
-      listen_for /nested confirm/ do
-        confirm "Does confirm work?" do |confirmed|
-          if confirmed 
-            say "Confirmed"
-            confirm "What about inside itself?" do |confirmed2|
-              if confirmed2
-                say "Confirmed2"
-              else
-                say "Canceled2"
-              end
-            end
-          else
-            say "Canceled"
-          end
-        end
-      end
-
-      listen_for /sequential confirm/ do
-        confirm "Does confirm work?" do |confirmed|
-          if confirmed 
-            say "Confirmed"
-          else
-            say "Canceled"
-          end
-        end
-        confirm "And a second time?" do |confirmed|
-          if confirmed 
-            say "Confirmed2"
-          else
-            say "Canceled2"
-          end
-        end
-        confirm "And a third time?" do |confirmed|
-          if confirmed 
-            say "Confirmed3"
-          else
-            say "Canceled3"
-          end
-        end
-      end
-
-      listen_for /bar/, within_state: :waiting_for_bar do
-        say "bar get"
-      end
-
-      listen_for /my name is (\w+) (\w+)/i do |first_name, last_name|
-        say "Nice to meet you, #{first_name} #{last_name}"
-      end
-
-      listen_for /only repeat this/i do
-        say "#{match_data}"
-      end
-    end
-
     let(:plugin) do
       TestPlugin.new.tap { |plugin| plugin.manager = subject }
     end
@@ -300,6 +200,6 @@ describe Cora do
         subject.should_receive(:respond).with("only repeat this")
         subject.process("only repeat this, not this")
       end
-    end
+      end
   end
 end
